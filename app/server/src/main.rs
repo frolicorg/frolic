98a5097bd92881f0a0ec191e797414911e69a153
Arihant Parsoya <parsoyaarihant@gmail.com>
2023-07-26 22:47:06 +0530

    added sql table

--- app/server/src/main.rs
+++ app/server/src/main.rs
@@ -1,7 +1,9 @@
 use actix_web::{get, post, web, Result, App, HttpResponse, HttpServer, Responder};
+use std::{env};
 
 mod models;
 use models::RESTInputModel;
+
 mod query_engine;
 
 async fn rest_api(jsonQuery: web::Json<RESTInputModel>) -> Result<String> {
@@ -27,8 +29,47 @@ struct AppState {
     app_name: String,
 }
 
+fn get_conn_builder(
+    db_user: String,
+    db_password: String,
+    db_host: String,
+    db_port: u16,
+    db_name: String,
+) -> mysql::OptsBuilder {
+    mysql::OptsBuilder::new()
+        .ip_or_hostname(Some(db_host))
+        .tcp_port(db_port)
+        .db_name(Some(db_name))
+        .user(Some(db_user))
+        .pass(Some(db_password))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+    // initialize environment
+    dotenv::dotenv().ok();
+
+    // initialize logger
+    // env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    // log::info!("setting up app from environment");
+
+    let db_user = env::var("MYSQL_USER").expect("MYSQL_USER is not set in .env file");
+    let db_password = env::var("MYSQL_PASSWORD").expect("MYSQL_PASSWORD is not set in .env file");
+    let db_host = env::var("MYSQL_HOST").expect("MYSQL_HOST is not set in .env file");
+    let db_port = env::var("MYSQL_PORT").expect("MYSQL_PORT is not set in .env file");
+    let db_name = env::var("MYSQL_DBNAME").expect("MYSQL_DBNAME is not set in .env file");
+    let db_port = db_port.parse().unwrap();
+
+    let builder = get_conn_builder(db_user, db_password, db_host, db_port, db_name);
+
+    // log::info!("initializing database connection");
+
+    let pool = mysql::Pool::new(builder).unwrap();
+
+    let shared_data = web::Data::new(pool);
+
     HttpServer::new(|| {
         App::new()
             .app_data(web::Data::new(AppState {
