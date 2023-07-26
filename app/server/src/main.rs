6e2c2e5ed97dfd520f9f0ca3f919c4c53afd019d
Arihant Parsoya <parsoyaarihant@gmail.com>
2023-07-26 20:45:05 +0530

    Update main.rs

--- app/server/src/main.rs
+++ app/server/src/main.rs
@@ -4,32 +4,23 @@ use actix_web::{get, post, web, Result, App, HttpResponse, HttpServer, Responder
 mod models;
 use models::RESTInputModel;
 
+/// extract `Info` using serde
+async fn rest_api(info: web::Json<RESTInputModel>) -> Result<String> {
+    Ok(format!("Welcome {}!", info.Metrics[0].Field))
+}
+
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
 }
 
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
-// #[derive(Deserialize)]
-// struct Info {
-//     username: String,
-// }
-
-/// extract `Info` using serde
-async fn index(info: web::Json<RESTInputModel>) -> Result<String> {
-    Ok(format!("Welcome {}!", info.Metrics[0].Field))
-}
-
-struct AppState {
-    app_name: String,
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
 }
 
 #[actix_web::main]
@@ -41,8 +32,8 @@ async fn main() -> std::io::Result<()> {
             }))
             .service(hello)
             .service(echo)
-            .route("/test", web::post().to(index))
-            .route("/hey", web::get().to(manual_hello))
+            .route("/api", web::post().to(rest_api))
+            .route("/hello", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
