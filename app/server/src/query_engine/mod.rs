3c827b2b12065a28c3312c581e6411e56a70daeb
rgargrajatgarg <rgargrajatgarg@gmail.com>
2023-07-26 21:33:52 +0530

    added the function to evaluate metrics

--- app/server/src/query_engine/mod.rs
+++ app/server/src/query_engine/mod.rs
@@ -1,6 +1,23 @@
 use crate::models;
+use models::Metric;
 
 pub fn GetQuery(query: &models::RESTInputModel) -> String {
     let result = "Hello from my_function!".to_string();
-    query.Metrics[0].Field.to_string()
+    // query.Metrics[0].Field.to_string()
+    metrics_to_sql_columns(&query.Metrics)
+}
+
+// Function to convert metrics to SQL columns string.
+pub fn metrics_to_sql_columns(metrics: &Vec<Metric>) -> String {
+    let mut sql_columns = Vec::new();
+    let valid_aggregations = ["sum", "avg", "count", "max", "min"]; 
+    for metric in metrics {
+        if valid_aggregations.contains(&metric.AggregateOperator.as_str()) {
+            let column_sql = format!("{}({})", metric.AggregateOperator.to_uppercase(), metric.Field);
+            sql_columns.push(column_sql);
+        } else {
+            eprintln!("Unknown aggregation function for column '{}'", metric.Field);
+        }
+    }
+    sql_columns.join(", ")
 }
