1d1252a7e0ca522c9f56a15ff1f03443b1dfe3bf
rgargrajatgarg <rgargrajatgarg@gmail.com>
2023-07-31 14:52:52 +0530

    filter bug fix
    filters working for non string columns as well

--- app/server/src/query_engine/mod.rs
+++ app/server/src/query_engine/mod.rs
@@ -4,10 +4,12 @@ use std::collections::HashMap;
 
 pub fn get_query(query: &models::RESTInputModel, tables: &Vec<Table>) -> String {
     let tables_json = serde_json::to_string_pretty(&tables).unwrap();
-    log::info!("{}", tables_json);
+    // log::info!("{}", tables_json);
 
-    // let tables: tables;
+    //get the tables setup
     let tables: Vec<Table> = tables.iter().cloned().collect();
+
+    //fetch the columns requested by user
     let metric_fields: Vec<String> = query
         .metrics
         .iter()
@@ -18,24 +20,43 @@ pub fn get_query(query: &models::RESTInputModel, tables: &Vec<Table>) -> String 
         .iter()
         .map(|metric| metric.field.clone())
         .collect();
-    let metrics_sql = metrics_to_sql(&query.metrics);
-    let dimensions_sql = dimensions_to_sql(&query.dimensions);
+
     let mut filter_fields = Vec::new();
-    let filters = if let Some(filters) = &query.filters {
+    if let Some(filters) = &query.filters {
         filter_fields = filters
             .iter()
             .map(|filter| filter.dimension.field.clone())
             .collect();
-        filters_to_sql(filters)
-    } else {
-        String::new()
-    };
+    }
+
     //this field captures all the column fields that are requested by the user
     let mut all_fields: Vec<String> = Vec::new();
     all_fields.extend(metric_fields);
     all_fields.extend(dimesion_fields);
     all_fields.extend(filter_fields);
+    
+    //check if the columns are present in the tables or not, if present create a hashmap as well to get the datatype
+    let mut field_datatype_map: HashMap<&String, &str> = HashMap::new();
+    for field in &all_fields{
+        match find_column_datatype(&tables, field) {
+            Some(datatype) => {
+                println!("Column datatype: {}", datatype);
+                field_datatype_map.insert(field, datatype);
+            }
+            None => return format!("Column : {} not found or invald input format",field)
+        }
+    }
+
+    //get the sql for each part separately
+    let metrics_sql = metrics_to_sql(&query.metrics);
+    let dimensions_sql = dimensions_to_sql(&query.dimensions);
+    let filters_sql = if let Some(filters) = &query.filters {
+        filters_to_sql(filters,&field_datatype_map)
+    } else {
+        String::new()
+    };
     let select_table_sql = all_fields.join(", ");
+
     //get all the table names requested by the user & process them
     let required_table_names = extract_table_columns(all_fields);
     let table_sql = handle_required_table(tables, required_table_names);
@@ -43,15 +64,14 @@ pub fn get_query(query: &models::RESTInputModel, tables: &Vec<Table>) -> String 
     //generate final mysql query
     format!(
         "select {}, {} from {} {} group by {} ;",
-        metrics_sql, dimensions_sql, table_sql, filters, dimensions_sql
+        metrics_sql, dimensions_sql, table_sql, filters_sql, dimensions_sql
     )
 }
 
 //this function takes required tables and registered tables and provide the joined table
 pub fn handle_required_table(
     registered_table: Vec<Table>,
-    required_table_names: Vec<String>,
-) -> String {
+    required_table_names: Vec<String>,) -> String {
     //from the tables vector filter the required tables to join
 
     //check if required tables are registered or not
@@ -152,17 +172,40 @@ pub fn dimensions_to_sql(dimensions: &Vec<Dimension>) -> String {
     sql_columns.join(", ")
 }
 
-pub fn filters_to_sql(filters: &Vec<Filter>) -> String {
+pub fn filters_to_sql(filters: &Vec<Filter>,field_datatype_map: &HashMap<&String, &str>) -> String {
     let mut sql_filters = Vec::new();
     let valid_operators = [">", "<", "="];
     for filter in filters {
+        let mut datatype_field = String::new();
+        match field_datatype_map.get(&filter.dimension.field.to_string()) {
+            Some(datatype) => datatype_field = datatype.to_string(),
+            None => return format!("Field '{}' not found in the map", &filter.dimension.field),
+        }
+        // Choose the format string based on the datatype
+        // let format_string = match datatype_field.as_str() {
+        //     "varchar" => "{} {} \"{}\"",
+        //     "int" | "bigint" => "{} {} {}",
+        //     _ => panic!("Unsupported datatype"), // Add appropriate handling for other datatypes if needed
+        // };
         if valid_operators.contains(&filter.filter_operator.as_str()) {
-            let filter_sql = format!(
-                "{} {} \"{}\"",
-                dimensions_to_sql(&vec![filter.dimension.clone()]),
-                filter.filter_operator.to_uppercase(),
-                filter.filter_value
-            );
+
+
+            let filter_sql = match datatype_field.as_str(){
+                "varchar" | "datetime" => format!(
+                    "{} {} \"{}\"",
+                    dimensions_to_sql(&vec![filter.dimension.clone()]),
+                    filter.filter_operator.to_uppercase(),
+                    filter.filter_value
+                ),
+                "int" | "bigint" | "float" => format!(
+                    "{} {} {}",
+                    dimensions_to_sql(&vec![filter.dimension.clone()]),
+                    filter.filter_operator.to_uppercase(),
+                    filter.filter_value
+                ),
+                _ => panic!("Unsupported datatype"),
+            };
+
             sql_filters.push(filter_sql)
         } else {
             eprintln!(
@@ -170,25 +213,6 @@ pub fn filters_to_sql(filters: &Vec<Filter>) -> String {
                 filter.dimension.field
             );
         }
-
-        // match &filter.FilterOperator {
-        //     Some(operator) => {
-        //         let uppercase_aggregate = operator.to_uppercase();
-        //         let aggregate_str = operator.as_str();
-        //         println!("{}", aggregate_str);
-
-        //         if valid_aggregations.contains(&aggregate_str) {
-        //             let column_sql = format!("{}({})", uppercase_aggregate, metric.Field);
-        //             sql_columns.push(column_sql);
-        //         } else {
-        //             eprintln!("Unknown aggregation function for column '{}'", metric.Field);
-        //         }
-        //     }
-        //     None => {
-        //         let column_sql = format!("({})", metric.Field);
-        //         sql_columns.push(column_sql);
-        //     }
-        // }
     }
     format!("where {}",sql_filters.join(" and "))
 }
@@ -242,4 +266,25 @@ fn generate_join_query(tables: &[Table]) -> Option<String> {
     }
 
     Some(query)
+}
+
+
+pub fn find_column_datatype<'a>(tables: &'a [Table], column_name: &'a str) -> Option<&'a str> {
+    // Split the input string into table name and column name
+    let parts: Vec<&str> = column_name.split('.').collect();
+    if parts.len() != 2 {
+        return None; // The input string should be in the format 'table.column_name'
+    }
+
+    let table_name = parts[0];
+    let column_name = parts[1];
+
+    // Find the table with the given name
+    let table = tables.iter().find(|t| t.name == table_name)?;
+
+    // Find the column with the given name in the table's columns
+    let column = table.columns.iter().find(|c| c.name == column_name)?;
+
+    // Return the datatype of the found column
+    Some(&column.datatype)
 }
