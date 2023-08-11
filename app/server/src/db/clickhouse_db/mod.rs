531f6d10e603ae13507307c0b339b61c54a388c6
rgargrajatgarg <rgargrajatgarg@gmail.com>
2023-08-11 11:32:41 +0530

    clickhouse support to be added

--- app/server/src/db/clickhouse_db/mod.rs
+++ app/server/src/db/clickhouse_db/mod.rs
@@ -1,11 +1,69 @@
-use clickhouse_rs::Pool;
-pub fn clickhouse_pool_builder(db_user:&str,db_password:&str,db_host:&str,db_port:&u16,db_name:&str) -> clickhouse_rs::Pool{
+// use crate::db::DBPool;
+// use crate::models::{AttributeValue,Column,DataResponse};
+// use crate::db_utils::PersistenceError;
 
-    let database_url = format!(
-        "clickhouse://{}:{}@{}:{}/{}",
-        db_user, db_password, db_host, db_port, db_name
-    );
-    let pool = Pool::new(database_url);
-    // let pool = mysql::Pool::new(builder).unwrap();
-    pool
-}
+// use std::collections::HashMap;
+// use std::{env, error::Error};
+// use futures_util::StreamExt;
+// use serde::Deserialize;
+
+// use crate::db::{ClickhouseClientWrapper};
+// use clickhouse::Client;
+// use clickhouse::Row;
+
+
+// #[derive(Row, Deserialize)]
+// struct MyRow<'a> {
+//     no: u32,
+//     name: &'a str,
+// }
+
+// pub fn clickhouse_pool_builder(db_user:&str,db_password:&str,db_host:&str,db_port:&u16,db_name:&str) -> ClickhouseClientWrapper{
+//     let database_url = format!(
+//         "clickhouse://{}:{}",
+//         db_host, db_port
+//     );
+//     let client = Client::default()
+//     .with_url("http://localhost:8123")
+//     .with_user(db_user)
+//     .with_password(db_password)
+//     .with_database(db_name);
+//     ClickhouseClientWrapper(client)
+// }
+// pub fn get_clickhouse_pool(dbpool: &DBPool) -> Option<&ClickhouseClientWrapper> {
+//     match dbpool {
+//         DBPool::clickhouse(clickhouse_pool) => Some(clickhouse_pool),
+//         _ => None,
+//     }
+// }
+
+// pub async fn run_query_clickhouse(
+//     column_headers: &Vec<String>,
+//     query: &String,
+//     pool: DBPool,
+// ) -> Result<DataResponse, PersistenceError>{
+//     log::info!("Executing PostGres Query");
+
+//     if let Some(clickhouse_pool) = get_clickhouse_pool(&pool) {
+
+//         let mut cursor = clickhouse_pool.unwrap()
+//         .query("SELECT ?fields FROM some WHERE no BETWEEN ? AND ?")
+//         .bind(500)
+//         .bind(504)
+//         .fetch::<MyRow<'_>>()?;
+    
+//         while let Some(row) = cursor.next().await? { 
+//             println!("Printing");
+//         }
+//         // let stmt = client.prepare_cached(query).await.unwrap();
+//         // let rows = client.query(&stmt, &[]).await.unwrap();
+//         // let column_head: Vec<String> = vec!["id".to_string(), "title".to_string()];
+
+//         let mut hash_map: HashMap<String, AttributeValue> = HashMap::new();
+//         let response_data = hash_map;
+//         return Ok(DataResponse { data: response_data });
+//     }
+//     else{
+//         Err(PersistenceError::Unknown)
+//     }
+// }
--- app/server/src/db/mod.rs
+++ app/server/src/db/mod.rs
@@ -10,22 +10,43 @@ use memcache::Client;
 use crate::db_utils::PersistenceError;
 use std::collections::HashMap;
 use log::error;
+use std::fmt;
 
-use clickhouse_rs::{Pool as clickhouse_pool};
+// use clickhouse::{Client as ClickhouseClient};
 use postgres::{postgres_pool_builder,fetch_all_tables_postgres,fetch_all_columns_postgres,run_query_postgres};
 use mysql_db::{mysql_pool_builder,fetch_all_tables_mysql,fetch_all_columns_mysql,run_query_mysql};
-use clickhouse_db::{clickhouse_pool_builder};
+// use clickhouse_db::{clickhouse_pool_builder};
 
 // use tokio::runtime;
 
+// pub struct ClickhouseClientWrapper(ClickhouseClient);
 
+// impl ClickhouseClientWrapper {
+//     // Method to unwrap and get the inner clickhouse::Client
+//     pub fn unwrap(self) -> ClickhouseClient {
+//         self.0
+//     }
+// }
+// impl fmt::Debug for ClickhouseClientWrapper {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         // Format the wrapped ClickhouseClient instance here
+//         write!(f, "ClickhouseClientWrapper {{ /* details here */ }}")
+//     }
+// }
+
+// impl Clone for ClickhouseClientWrapper {
+//     fn clone(&self) -> Self {
+//         ClickhouseClientWrapper(self.0.clone())
+//     }
+// }
 #[derive(Debug, Clone)]
 pub enum DBPool {
     mysql(mysql::Pool),
     postgres(deadpool_postgres::Pool),
-    clickhouse(clickhouse_pool),
+    // clickhouse(ClickhouseClientWrapper),
     None
 }
+
 
 impl DBPool{
     pub fn new()->DBPool{
@@ -45,11 +66,11 @@ pub fn pool_builder(db_type:&str,db_user:&str,db_password:&str,db_host:&str,db_p
             let db_pool = DBPool::mysql(pool);
             Ok(db_pool)
         },
-        "clickhouse" => {
-            let pool = clickhouse_pool_builder(db_user, db_password, db_host, db_port, db_name);
-            let db_pool = DBPool::clickhouse(pool);
-            Ok(db_pool)
-        }
+        // "clickhouse" => {
+        //     let pool = clickhouse_pool_builder(db_user, db_password, db_host, db_port, db_name);
+        //     let db_pool = DBPool::clickhouse(pool);
+        //     Ok(db_pool)
+        // }
         _ => Err("Unsupported database type".to_string())
     }
 }
